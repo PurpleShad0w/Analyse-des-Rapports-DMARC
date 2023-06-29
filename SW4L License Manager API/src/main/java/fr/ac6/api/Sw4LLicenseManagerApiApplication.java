e6d32c0af721ab705b23ef66ffe1d010ba9f7dc0
Octave Cinquanta <thepurpl3shad0w@gmail.com>
2023-06-29 11:57:28 +0200

    Insert API Template

---
+++ SW4L License Manager API/src/main/java/fr/ac6/api/Sw4LLicenseManagerApiApplication.java
@@ -0,0 +1,13 @@
+package fr.ac6.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sw4LLicenseManagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sw4LLicenseManagerApiApplication.class, args);
+	}
+
+}
---
+++ SW4L License Manager API/src/test/java/fr/ac6/api/Sw4LLicenseManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.ac6.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sw4LLicenseManagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
