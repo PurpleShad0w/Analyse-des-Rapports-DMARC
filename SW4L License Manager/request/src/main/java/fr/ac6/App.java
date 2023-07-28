5ba745b2d4a4a304179a519f8e42f48e337d3c76
Octave Cinquanta <thepurpl3shad0w@gmail.com>
2023-07-28 17:17:43 +0200

    Java function now works with parameters Call with request path and dictionary of JSON parameters

--- SW4L License Manager/request/src/main/java/fr/ac6/App.java
+++ SW4L License Manager/request/src/main/java/fr/ac6/App.java
@@ -2,9 +2,11 @@ package fr.ac6;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.UnsupportedEncodingException;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.Dictionary;
+import java.util.Enumeration;
+import java.util.Hashtable;
 import java.util.List;
 import org.apache.commons.io.IOUtils;
 import org.apache.http.HttpEntity;
@@ -19,16 +21,29 @@ import org.apache.http.message.BasicNameValuePair;
 
 public class App 
 {
-    public static void main( String[] args ) throws IOException, UnsupportedEncodingException
+    public static void main(String[] args) throws IOException {
+        test();
+    }
+
+    public static void request(String path, Dictionary<String, String> parameters) throws IOException
     {
         HttpClient httpclient = HttpClients.createDefault();
-        HttpPost httppost = new HttpPost("http://localhost/post/post.php");
+        String default_path = "http://localhost/post/";
+        String host_path = default_path.concat(path);
+        HttpPost httppost = new HttpPost(host_path);
         
-        List<NameValuePair> params = new ArrayList<NameValuePair>(2);
-        params.add(new BasicNameValuePair("macAddress", "mac_test"));
-        params.add(new BasicNameValuePair("licenseFeature", "feature_test"));
+        Integer dict_size = parameters.size();
+        List<NameValuePair> params = new ArrayList<NameValuePair>(dict_size);
+        Enumeration<String> k = parameters.keys();
+
+        while (k.hasMoreElements()) {
+            String key = k.nextElement();
+            String value = parameters.get(key);
+            params.add(new BasicNameValuePair(key, value));
+        }
+        
         httppost.setEntity(new UrlEncodedFormEntity(params, "UTF-8"));
-        
+
         HttpResponse response = httpclient.execute(httppost);
         HttpEntity entity = response.getEntity();
 
@@ -38,5 +53,15 @@ public class App
                 System.out.println(result);
             }
         }
+    }
+
+    public static void test() throws IOException
+    {
+        String test_path = "post.php";
+        Dictionary<String, String> test_parameters = new Hashtable<>();
+        test_parameters.put("macAddress", "mac_test");
+        test_parameters.put("licenseFeature", "feature_test");
+
+        request(test_path, test_parameters);
     }
 }
